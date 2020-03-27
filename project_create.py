aaccbe79468b2501c4869b7f79903064c7b15658
Neil <neil.tellez@gmail.com>
2020-03-27 15:09:42 -0400

    Using npx

--- project_create.py
+++ project_create.py
@@ -5,7 +5,5 @@ import subprocess
 def Init(project_name, repo_name):
   os.mkdir(project_name)
   os.chdir(project_name)
-  # Create README.md with title of project_name
-  subprocess.check_call("echo '# {}' >> README.md".format(repo_name), shell=True)
-  # creating react-app - may change to use npx instead of npm
-  subprocess.call("npm create-react-app .", shell=True)
+  # Creates react app
+  subprocess.call("npx create-react-app .", shell=True)
